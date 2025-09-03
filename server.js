554e9d5690ec039dc7101a443f9fde17404e6cbd
crowrooster971 <wanlishajna@hotmail.com>
2025-09-03 20:06:55 +0300

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/système-de-retours-utilisateur', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur le Système de Retours Utilisateur !');
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur en cours d\'exécution sur le port ${PORT}`);
+});
